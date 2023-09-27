8e81f308b270b667bd10eb0d4edd5ca61cee71a1
juan.zea <juan.zea@innclod.com>
2023-09-26 22:19:11 -0500

    First State

---
+++ src/test/java/com/luv2code/springboot/crud_demo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.crud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
